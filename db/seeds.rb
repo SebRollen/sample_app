c0e244e39d22b261ddc5345a5d919fe6729b012c
Sebastian Rollen <seb@overmu.se>
2022-03-06 12:02:41 -0500

    Add account activation

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -14,5 +16,6 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password,
-               admin: false)
+               activated: true,
+               activated_at: Time.zone.now)
 end
