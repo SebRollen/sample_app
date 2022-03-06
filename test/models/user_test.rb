1da0d50ee5e6563d00525481a39d779baade85d2
Sebastian Rollen <seb@overmu.se>
2022-03-06 17:46:03 -0500

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -81,4 +81,40 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test "should follow and unfollow a user" do
+    test_user = users(:test_user)
+    archer  = users(:archer)
+    assert_not test_user.following?(archer)
+    test_user.follow(archer)
+    assert test_user.following?(archer)
+    assert archer.followers.include?(test_user)
+    test_user.unfollow(archer)
+    assert_not test_user.following?(archer)
+    # Users can't follow themselves.
+    test_user.follow(test_user)
+    assert_not test_user.following?(test_user)
+  end
+
+  test "feed should have the right posts" do
+    test_user = users(:test_user)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert test_user.feed.include?(post_following)
+    end
+    # Self-posts for user with followers
+    test_user.microposts.each do |post_self|
+      assert test_user.feed.include?(post_self)
+    end
+    # Self-posts for user with no followers
+    archer.microposts.each do |post_self|
+      assert archer.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not test_user.feed.include?(post_unfollowed)
+    end
+  end
 end
